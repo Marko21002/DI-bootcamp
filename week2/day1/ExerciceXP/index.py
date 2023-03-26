17e5bc8468987c553b2f6eb9c29a081bfdaf75df
Marko21002 <markgufman21@gmail.com>
2023-03-26 13:48:24 +0300

    ex xp done

--- week2/day1/ExerciceXP/index.py
+++ week2/day1/ExerciceXP/index.py
@@ -1,1 +1,48 @@
+
 print("Hello world\n"*4)
+
+# Exercice2
+
+mar = (99**3)*8
+print(mar)
+
+# Exercice 4
+
+computer_brand = 'Mac'
+sentens = f'I have a {computer_brand}'
+print(sentens)
+
+# Exercice 5
+name = 'Marko'
+age = '20'
+shoe_size = '42'
+sentens1 = f'Hello i am {name} my age is {age} my shoe size is {shoe_size}'
+print(sentens1)
+
+# Exercice 6
+a = int(input())
+b = int(input())
+
+if a > b:
+    print('Hello world')
+
+# Exercice 7
+num = int(input())
+if num % 2 == 0:
+    print('Odd')
+else:
+    print('Even')
+
+# Exercice 8
+urname = input()
+if name == urname:
+    print('Same')
+else:
+    print('not same')
+
+# Exercice 9
+urheight = int(input())
+if urheight >= 145:
+    print('U can')
+else:
+    print('Gnome')
